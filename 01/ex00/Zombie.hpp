2067618e430a4192dc6352819c3f6ba44ed8cb24
adouiyeh <adouiyeh@e1r9p9.1337.ma>
2025-05-11 02:28:35 +0100

    cpp 01 in progress

---
+++ 01/ex00/Zombie.hpp
@@ -0,0 +1,15 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie 
+{
+    private:
+        std::string _name;
+    
+    public:
+        void announce(void);
+};
+
+#endif
